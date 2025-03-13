f5c4d963810ef9caadcce87ec0f517bc799de536
Adam Simon <adamsimon@sweetpunk.com>
2025-03-13 10:02:28 +0100

    removed useless use effefct in the translation

--- src/components/LanguageSelector/LanguageSelector.tsx
+++ src/components/LanguageSelector/LanguageSelector.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useLanguage } from "../../context/LanguageContext";
 import styles from "./LanguageSelector.module.css";
 
@@ -11,11 +11,11 @@ import FlagSpain from "./FlagSvg/FlagSpain";
 
 export default function LanguageSelector() {
   const { selectedLanguage, setSelectedLanguage } = useLanguage();
-  const [showFlags, setShowFlags] = useState(true);
-  const [showFlags2, setShowFlags2] = useState(true);
+  const [showFlags, setShowFlags] = useState(false);
+  const [showFlags2, setShowFlags2] = useState(false);
   const [flagSide, setFlagSide] = useState(false);
 
-  useEffect(() => {
+  const toggleFlags = () => {
     if (flagSide) {
       setFlagSide(false);
       setShowFlags(!showFlags);
@@ -29,70 +29,32 @@ export default function LanguageSelector() {
         setShowFlags(!showFlags);
       }, 100);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selectedLanguage]);
+  };
 
-  const changeLanguage = (language: string) => {
-    setSelectedLanguage(language as "en" | "fr" | "de" | "es" | "it");
+  const changeLanguage = (language: "en" | "fr" | "de" | "es" | "it") => {
+    setSelectedLanguage(language);
+    toggleFlags();
   };
 
   return (
     <div className={`${styles.btLanguageContainer}`}>
       <button
         type="button"
-        onClick={() => {
-          if (flagSide) {
-            setFlagSide(false);
-            setShowFlags(!showFlags);
-            setTimeout(() => {
-              setShowFlags2(!showFlags2);
-            }, 100);
-          } else {
-            setFlagSide(true);
-            setShowFlags2(!showFlags2);
-            setTimeout(() => {
-              setShowFlags(!showFlags);
-            }, 100);
-          }
-        }}
+        onClick={toggleFlags}
         className="MouseHoverEffect"
       >
-        {selectedLanguage === "fr" && (
-          <div>
-            <FlagFrance aria-label="French flag" />
-          </div>
-        )}
-        {selectedLanguage === "en" && (
-          <div>
-            <FlagEngland aria-label="England flag" />
-          </div>
-        )}
-        {selectedLanguage === "de" && (
-          <div>
-            <FlagGermany aria-label="German flag" />
-          </div>
-        )}
-        {selectedLanguage === "es" && (
-          <div>
-            <FlagSpain aria-label="Spanish flag" />
-          </div>
-        )}
-        {selectedLanguage === "it" && (
-          <div>
-            <FlagItaly aria-label="Italian flag" />
-          </div>
-        )}
+        {selectedLanguage === "fr" && <FlagFrance aria-label="French flag" />}
+        {selectedLanguage === "en" && <FlagEngland aria-label="England flag" />}
+        {selectedLanguage === "de" && <FlagGermany aria-label="German flag" />}
+        {selectedLanguage === "es" && <FlagSpain aria-label="Spanish flag" />}
+        {selectedLanguage === "it" && <FlagItaly aria-label="Italian flag" />}
       </button>
       <button
         type="button"
         onClick={() => changeLanguage("fr")}
         className={`${
-          showFlags2 && selectedLanguage !== "fr"
-            ? styles.flagDisplay
-            : styles.flagHidden
-        } ${
-          showFlags && selectedLanguage !== "fr" ? "" : styles.dpnone
-        } MouseHoverEffect`}
+          showFlags2 && selectedLanguage !== "fr" ? styles.flagDisplay : styles.flagHidden
+        } ${showFlags && selectedLanguage !== "fr" ? "" : styles.dpnone} MouseHoverEffect`}
       >
         <FlagFrance aria-label="French flag" />
       </button>
@@ -100,12 +62,8 @@ export default function LanguageSelector() {
         type="button"
         onClick={() => changeLanguage("en")}
         className={`${
-          showFlags2 && selectedLanguage !== "en"
-            ? styles.flagDisplay
-            : styles.flagHidden
-        } ${
-          showFlags && selectedLanguage !== "en" ? "" : styles.dpnone
-        } MouseHoverEffect`}
+          showFlags2 && selectedLanguage !== "en" ? styles.flagDisplay : styles.flagHidden
+        } ${showFlags && selectedLanguage !== "en" ? "" : styles.dpnone} MouseHoverEffect`}
       >
         <FlagEngland aria-label="England flag" />
       </button>
@@ -113,12 +71,8 @@ export default function LanguageSelector() {
         type="button"
         onClick={() => changeLanguage("de")}
         className={`${
-          showFlags2 && selectedLanguage !== "de"
-            ? styles.flagDisplay
-            : styles.flagHidden
-        } ${
-          showFlags && selectedLanguage !== "de" ? "" : styles.dpnone
-        } MouseHoverEffect`}
+          showFlags2 && selectedLanguage !== "de" ? styles.flagDisplay : styles.flagHidden
+        } ${showFlags && selectedLanguage !== "de" ? "" : styles.dpnone} MouseHoverEffect`}
       >
         <FlagGermany aria-label="German flag" />
       </button>
@@ -126,12 +80,8 @@ export default function LanguageSelector() {
         type="button"
         onClick={() => changeLanguage("es")}
         className={`${
-          showFlags2 && selectedLanguage !== "es"
-            ? styles.flagDisplay
-            : styles.flagHidden
-        } ${
-          showFlags && selectedLanguage !== "es" ? "" : styles.dpnone
-        } MouseHoverEffect`}
+          showFlags2 && selectedLanguage !== "es" ? styles.flagDisplay : styles.flagHidden
+        } ${showFlags && selectedLanguage !== "es" ? "" : styles.dpnone} MouseHoverEffect`}
       >
         <FlagSpain aria-label="Spanish flag" />
       </button>
@@ -139,12 +89,8 @@ export default function LanguageSelector() {
         type="button"
         onClick={() => changeLanguage("it")}
         className={`${
-          showFlags2 && selectedLanguage !== "it"
-            ? styles.flagDisplay
-            : styles.flagHidden
-        } ${
-          showFlags && selectedLanguage !== "it" ? "" : styles.dpnone
-        } MouseHoverEffect`}
+          showFlags2 && selectedLanguage !== "it" ? styles.flagDisplay : styles.flagHidden
+        } ${showFlags && selectedLanguage !== "it" ? "" : styles.dpnone} MouseHoverEffect`}
       >
         <FlagItaly aria-label="Italian flag" />
       </button>
