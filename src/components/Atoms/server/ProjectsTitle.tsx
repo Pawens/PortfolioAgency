7e33c803b2bc241bfa805095f386299f10899bbb
PARISOT Romain <romainparisot.pro@gmail.com>
2025-08-27 20:38:09 +0200

    feat: responsive adapted to all languages

--- src/components/Atoms/server/ProjectsTitle.tsx
+++ src/components/Atoms/server/ProjectsTitle.tsx
@@ -6,7 +6,7 @@ import { Language } from "@/context/LanguageContext";
 export default function ProjectsTitle({ language }: { language: Language }) {
   return (
     <div
-      className="project-title-container flex flex-col items-center"
+      className="project-title-container flex flex-col items-center w-full"
       style={{
         backgroundImage: "url('/backgroundLightToDark.png')",
         backgroundSize: "100% 101%",
@@ -17,7 +17,7 @@ export default function ProjectsTitle({ language }: { language: Language }) {
       }}
     >
       <h2
-        className="projects-title text-[360px] font-bold uppercase pt-[64px]"
+        className="projects-title text-[260px] font-bold uppercase pt-[64px]"
         style={{
           background:
             "linear-gradient(180deg, rgba(45, 83, 111, 0.99) 0%, rgba(124, 164, 192, 1) 100%)",
