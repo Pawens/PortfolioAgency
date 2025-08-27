c55fd4faaddead50996b18b23e8432d6ea54ad5e
PARISOT Romain <romainparisot.pro@gmail.com>
2025-08-27 20:03:28 +0200

    feat: footer responsive

--- src/components/Organisms/server/Footer.tsx
+++ src/components/Organisms/server/Footer.tsx
@@ -5,16 +5,18 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="flex flex-col justify-between gap-[32px] px-[88px] pb-[32px]">
-      <div className="flex self-end">
+    <div className="footer-container flex flex-col justify-between gap-[32px] px-[88px] pb-[32px]">
+      <div className="footer-button-backtotop flex self-end">
         <ButtonDefault variant="footer" type="backToTop" href="#">
           <ArrowPawensBig />
         </ButtonDefault>
       </div>
       <span className="w-full h-[1px] bg-[var(--color-secondary)]" />
       <div className="flex items-center justify-between">
-        <p className="text-[var(--color-secondary)]">Copyright. Pawens @2025</p>
-        <p className="text-[var(--color-secondary)]">
+        <p className="footer-text text-[var(--color-secondary)]">
+          Copyright. Pawens @2025
+        </p>
+        <p className="footer-text text-[var(--color-secondary)]">
           Parisot Romain, Simon Adam
         </p>
       </div>
