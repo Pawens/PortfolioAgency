a4450efceb114176fbbec2c7239bd2119eb08fe4
PARISOT Romain <romainparisot.pro@gmail.com>
2025-08-28 22:18:47 +0200

    fix: responsive svg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Footer from "@/components/Organisms/server/Footer";
 import LogoFooter from "@/assets/icons/LogoFooter.svg";
 import BackgroundProcess from "@/assets/icons/BackgroundProcess.svg";
 import Review from "@/components/Organisms/server/Reviews";
-// import Projects from "@/components/Organisms/server/Projects";
+import Projects from "@/components/Organisms/server/Projects";
 import Services from "@/components/Organisms/server/Services";
 import Process from "@/components/Organisms/server/Process";
 import { getLanguageFromSearchParams } from "@/utils/serverTranslations";
@@ -30,7 +30,7 @@ export default function Home({ searchParams }: Props) {
             <BackgroundProcess className="w-full h-full object-cover" />
           </div>
           <div className="relative z-10">
-            {/* <Projects language={language} /> */}
+            <Projects language={language} />
             <Services language={language} />
             <Process language={language} />
             <Review language={language} />
@@ -38,12 +38,12 @@ export default function Home({ searchParams }: Props) {
         </section>
 
         <section className="relative bg-gradient-to-t from-[#193345] to-[var(--color-black)]">
-          {/* <div
-            className="absolute z-10 pointer-events-none"
+          <div
+            className="absolute z-10 pointer-events-none max-w-[100vw] overflow-hidden"
             style={{ bottom: 0, left: 0 }}
           >
             <LogoFooter />
-          </div> */}
+          </div>
           <div className="relative z-20">
             <WhoAreWe />
             <FaqSection language={language} />
